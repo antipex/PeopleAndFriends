d7d2ce7c1a8fdf5059924a7d0790c34c8b9ce8d1
Kyle Rohr <public@antipex.com>
2020-09-21 08:58:19 -0700

    Initial Commit

---
+++ PeopleAndFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeopleAndFriends
+//
+//  Created by Kyle Rohr on 21/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
